127410d629d8aa2e93d9743af9b144e5dca3e82f
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-09-29 20:04:20 +0200

    Add project files.

---
+++ Kappitel 3 uppgift 3.16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång ät din sång?");
+            string låt = Console.ReadLine();
+
+            float musik = float.Parse(låt);
+
+            if (musik > 2.45 && musik < 4.20)
+            {
+                Console.WriteLine("Du får spela din låt");
+            }
+
+            else
+            {
+                Console.WriteLine("Du får tyvär inte spela din låt");
+            }
+        }
+    }
+}
